0bc5b05e6e8ae6666b10e062d1904d2dd7caf0fe
supersuhyeon <suhyeonkr@gmail.com>
2022-07-01 14:17:54 +0900

    menu 진행중

---
+++ MENU/main.js
@@ -0,0 +1,20 @@
+const menuIcon = document.querySelector('#menubtn')
+const rightMenuBar = document.querySelector('.rightmenubar')
+
+menuIcon.addEventListener('click', (e)=>{
+    if(rightMenuBar.classList.contains("active")) {
+        //메뉴 slide off
+        rightMenuBar.classList.remove('active')
+        document.getElementById('menubtn').src = "./menu_img/menuicon.png"
+        menuIcon.classList.add('btnreverse')
+        menuIcon.classList.remove('btnrotate')
+
+        
+    } else{
+        //메뉴 slide on
+        rightMenuBar.classList.add('active')
+        document.getElementById('menubtn').src = "./menu_img/menuicon2.png"
+        menuIcon.classList.add('btnrotate')
+        menuIcon.classList.remove('btnreverse')
+    }
+})
