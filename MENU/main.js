4e787f9461924a523785449d60839d11a4212bbc
supersuhyeon <suhyeonkr@gmail.com>
2022-07-02 15:28:57 +0900

    start monsquad intro

--- MENU/main.js
+++ MENU/main.js
@@ -31,3 +31,29 @@ new Swiper('.mySwiper', {
       },
   
 });
+
+//lodash_ throttle(함수,시간)
+//gsap.to() 애니메이션
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY)
+    if(window.scrollY > 500){
+        //hide
+        // badgeEl.style.display = 'none'
+        // gsap.to(요소,지속시간,옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0, //객체데이터형태로 넣어야함
+            display: 'none'
+        })
+
+    }else{
+        // show
+        // badgeEl.style.display = 'block'
+        gsap.to(badgeEl, .6, {
+            opacity: 1, //객체데이터형태로 넣어야함
+            display: 'block'
+        })
+    }
+},300)) //0.3s 단위로 함수 제어
