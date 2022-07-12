704771a583af47514ab24f3758ec8c383525ad59
supersuhyeon <suhyeonkr@gmail.com>
2022-07-12 12:31:00 +0900

    common file revising

--- ABOUT/about.js
+++ ABOUT/about.js
@@ -1,32 +1,3 @@
-const menuIcon = document.querySelector('#menubtn')
-const rightMenuBar = document.querySelector('.rightmenubar')
-
-menuIcon.addEventListener('click', (e)=>{
-    if(rightMenuBar.classList.contains("active")) {
-        //메뉴 slide off
-        rightMenuBar.classList.remove('active')
-        document.getElementById('menubtn').src = "/MENU/menu_img/menuicon.png"
-        menuIcon.classList.add('btnreverse')
-        menuIcon.classList.remove('btnrotate')
-
-        
-    } else{
-        //메뉴 slide on
-        rightMenuBar.classList.add('active')
-        document.getElementById('menubtn').src = "/MENU/menu_img/menuicon2.png"
-        menuIcon.classList.add('btnrotate')
-        menuIcon.classList.remove('btnreverse')
-    }
-})
-
-const mobileIcon = document.querySelector('.menuicon-mobile')
-const mobileDrop = document.querySelector('.dropmenu-mobile')
-
-mobileIcon.addEventListener('click', (e)=>{
-    mobileDrop.classList.toggle('active')
-})
-
-
 
 new Swiper('.mySwiper', {
     loop:true,
@@ -40,39 +11,6 @@ new Swiper('.mySwiper', {
       },
   
 });
-
-//lodash_ throttle(함수,시간)
-//gsap.to() 애니메이션
-
-
-window.addEventListener('scroll', _.throttle(function(){
-    console.log(window.scrollY)
-    if(window.scrollY > 500){
-        //hide
-        
-        // gsap.to(요소,지속시간,옵션);
-        gsap.to('#go-top',.2,{
-            x: 0,
-        })
-
-
-    }else{
-        // show
-        
-        gsap.to('#go-top',.2,{
-            x: 100,
-        })
-    }
-},300)) //0.3s 단위로 함수 제어
-
-const goTopEl = document.querySelector('#go-top')
-goTopEl.addEventListener('click', (e)=>{
-    // window.scrollTo(0,0)
-gsap.to(window,.7,{
-    scrollTo: 0
-}
-)
-})
 
 
 Sticker.init('.sticker');
--- MENU/main.js
+++ MENU/main.js
@@ -1,31 +1,3 @@
-const menuIcon = document.querySelector('#menubtn')
-const rightMenuBar = document.querySelector('.rightmenubar')
-
-menuIcon.addEventListener('click', (e)=>{
-    if(rightMenuBar.classList.contains("active")) {
-        //메뉴 slide off
-        rightMenuBar.classList.remove('active')
-        document.getElementById('menubtn').src = "./menu_img/menuicon.png"
-        menuIcon.classList.add('btnreverse')
-        menuIcon.classList.remove('btnrotate')
-
-        
-    } else{
-        //메뉴 slide on
-        rightMenuBar.classList.add('active')
-        document.getElementById('menubtn').src = "./menu_img/menuicon2.png"
-        menuIcon.classList.add('btnrotate')
-        menuIcon.classList.remove('btnreverse')
-    }
-})
-
-const mobileIcon = document.querySelector('.menuicon-mobile')
-const mobileDrop = document.querySelector('.dropmenu-mobile')
-
-mobileIcon.addEventListener('click', (e)=>{
-    mobileDrop.classList.toggle('active')
-})
-
 
 new Swiper('.mySwiper', {
     loop:true,
@@ -40,48 +12,6 @@ new Swiper('.mySwiper', {
   
 });
 
-//lodash_ throttle(함수,시간)
-//gsap.to() 애니메이션
-
-const badgeEl = document.querySelector('header .badges')
-
-const removeEvent = window.addEventListener('scroll', _.throttle(function(){
-    console.log(window.scrollY)
-    if(window.scrollY > 500){
-        //hide
-        // badgeEl.style.display = 'none'
-        // gsap.to(요소,지속시간,옵션);
-        gsap.to(badgeEl, .6, {
-            opacity: 0, //객체데이터형태로 넣어야함
-            display: 'none'
-        })
-        gsap.to('#go-top',.2,{
-            x: 0,
-        })
-
-
-    }else{
-        // show
-        // badgeEl.style.display = 'block'
-        gsap.to(badgeEl, .6, {
-            opacity: 1, //객체데이터형태로 넣어야함
-            display: 'block'
-        })
-        gsap.to('#go-top',.2,{
-            x: 100,
-        })
-    }
-},300)) //0.3s 단위로 함수 제어
-
-
-const goTopEl = document.querySelector('#go-top')
-goTopEl.addEventListener('click', (e)=>{
-    // window.scrollTo(0,0)
-gsap.to(window,.7,{
-    scrollTo: 0
-}
-)
-})
 
 
 function floatingObject(selector){
